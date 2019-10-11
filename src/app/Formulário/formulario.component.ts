db1b5b2c3de73a07185c903ac07112fdd803ff61
joel <joelgcampos@sapo.pt>
2019-10-11 11:02:21 +0100

    formulario
    10-10.

--- src/app/Formulário/formulario.component.ts
+++ src/app/Formulário/formulario.component.ts
@@ -1,33 +1,90 @@
 import { Component, OnInit } from '@angular/core';
-import { Questionário } from './reserva.model';
+import { Questionario } from './reserva.model';
 import { from } from 'rxjs';
+import { FormsService } from '../forms.service';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
-@Component({
+@Component ({
+
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
-  listaReservas: Array<Questionário> = [];
+  listaReservas: Questionario;
 
-  Primeiro_nome: string;
-  Apelido: string;
-  Email: string;
-  Endereco: string;
-  Codigo_postal: string;
-  País: string;
+  Nome_Clientes: string;
+  E_mail: string;
+  Morada_Clientes: string;
+  Localidade: string;
+  Codigo_postal: number;
+  Pais: string;
   Telefone: number;
 
+caminhoparaservice : FormsService;
 
-  addFormulario() {
-    // tslint:disable-next-line: max-line-length
-    this.listaReservas.push(new Questionário(this.Primeiro_nome, this.Apelido, this.Email, this.Endereco, this.Codigo_postal, this.País, this.Telefone));
-    console.log(this.listaReservas);
+constructor(private formBuilder: FormBuilder, private caminho : FormsService) {
+  this.caminhoparaservice = caminho;
+}
+
+questionarioForm: FormGroup;
+
+
+ngOnInit() {
+
+  this.questionarioForm = this.formBuilder.group({
+
+  Nome_Clientes: ['',Validators.required],
+  Morada_Clientes: ['',Validators.required],
+  E_mail: ['', Validators.required],
+  Localidade: ['', Validators.required],
+  Codigo_postal: ['', Validators.required],
+  Pais: ['', Validators.required],
+  Telefone: ['',Validators.required],
+});
   }
 
-  constructor() { }
+onSubmit() {
+console.log(this.questionarioForm);
+this.caminhoparaservice.createClientes(this.questionarioForm.value)
+  .subscribe(data => {
+   console.log(JSON.stringify(data));
+    //this.router.naviga
+
+});
+}
+
+}
+
+
+
+
+
+
+
+/*
+  formsService: any;
+  router: any;
+  addFormulario() {
+    // tslint:disable-next-line: max-line-length
+    this.listaReservas = new Questionario(this.Nome_Clientes, this.E_mail, this.Morada_Clientes, this.Localidade, this.Codigo_postal, this.Pais, this.Telefone);
+    //this.caminhoparaservice.createClientes(this.listaReservas);
+    //console.log(this.listaReservas);
+  }
+
+
 
   ngOnInit() {
   }
 
-}
+  onSubmit(){
+    console.log(this.addFormulario);
+    this.formsService.createClientes(this.addFormulario)
+      .subscribe(data=> {
+        console.log(data);
+        this.router.navigate(['Formulario']);
+
+      });
+          }
+    }
+*/
--- src/app/forms.service.ts
+++ src/app/forms.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ɵgetDebugNode__POST_R3__ } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {from} from 'rxjs'
+import { Questionario } from './Formulário/reserva.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormsService {
+clientesURL = 'http://localhost:5000/api/clientes';
+reservasURL = 'http://localhost:5000/api/reservas';
 
+  constructor(private http:HttpClient) { }
+  getReservas(){
+    return this.http.get<any[]>(this.reservasURL);
+  }
+createClientes(clientes: Questionario){
+  return this.http.post(this.clientesURL, clientes);
+}
 
-
-  constructor() { }
 }
